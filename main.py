30e30792d403f49fa223d2be798d5d531dc35d76
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-04-20 03:20:30 +0300

    added new function create_identical_file

--- main.py
+++ main.py
@@ -2,5 +2,14 @@
 def main():
     pass
 
+
+def create_identical_file(file_path: str, file_path_to_copy: str):
+    """receive existing file path and creates copy in file_path_to_copy"""
+    with open(file_path, "r") as file:
+        file_copy = file.read()
+    with open(file_path_to_copy, "w") as file:
+        file.write(file_copy)
+
+
 if __name__ == '__main__':
     main()
